b58b6fef8e9bbb3c53e9ee08a7936f3a85f83f4e
Naoman Saeed <naoman.saeed@gmail.com>
2025-12-26 06:45:52 +0500

    some tidying up.

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 const charset = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
     'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',0,1,2,3,4,5,6,7,8,9,'!','@','#','$','%','^','&','*','(',')']
 
+const uppers = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+const lowers = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+const numbers = [0,1,2,3,4,5,6,7,8,9]
+const symbols = ['!','@','#','$','%','^','&','*','(',')']
+
 let passOne = document.getElementById("password1")
 let passTwo = document.getElementById("password2")
 let generateBtn = document.getElementById("generate")
